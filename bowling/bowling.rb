e7bb05f044bd06dad832f463409fa56d4adfbba8
sokampdx <sokam@pdx.edu>
2017-04-20 22:37:47 -0700

    bowling : completed TDD bowling exercise

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -5,11 +5,45 @@ class Game
 	end
 
 	def roll(pin)
-		@rolls.push(pin)
+		@rolls[@current_roll] = pin
 		@current_roll += 1
 	end
 
 	def score
-		@rolls.inject(:+)
+    score = 0
+    first_in_frame = 0
+    (0...10).map do
+      if isStrike(first_in_frame)
+        score += 10 + nextTwoBallsAfterStrike(first_in_frame)
+        first_in_frame += 1
+      elsif isSpare(first_in_frame)
+        score += 10 + nextBallAfterSpare(first_in_frame)
+        first_in_frame += 2
+      else
+        score += twoBallsInFrame(first_in_frame) 
+        first_in_frame += 2
+      end
+    end
+    score
 	end	
+
+  def twoBallsInFrame(first_in_frame)
+    @rolls[first_in_frame] + @rolls[first_in_frame + 1]
+  end
+
+  def nextBallAfterSpare(first_in_frame)
+    @rolls[first_in_frame + 2]
+  end
+
+  def nextTwoBallsAfterStrike(first_in_frame)
+    @rolls[first_in_frame + 1] + @rolls[first_in_frame + 2]
+  end
+
+  def isStrike(first_in_frame)
+    @rolls[first_in_frame] == 10
+  end
+
+  def isSpare(first_in_frame)
+    (@rolls[first_in_frame] + @rolls[first_in_frame + 1]) == 10
+  end
 end
--- bowling/spec/bowling_spec.rb
+++ bowling/spec/bowling_spec.rb
@@ -6,6 +6,14 @@ describe Game do
 		nums.times { game.roll(pins) }
 	end
 
+  def roll_spare
+    roll_many(2, 5)
+  end
+
+  def roll_strike
+    game.roll(10)
+  end
+
 	let(:game) { Game.new }
 
 	it 'rolls all gutter' do
@@ -18,10 +26,29 @@ describe Game do
 		expect(game.score).to eq(20)
 	end
 
-	xit 'rolls one spare' do
-		roll_many(2, 5)
+	it 'rolls one spare' do
+    roll_spare
 		game.roll(3)
 		roll_many(17, 0)
 		expect(game.score).to eq(16)
 	end
+
+  it 'rolls one strike' do
+    roll_strike
+    game.roll(3)
+    game.roll(4)
+    roll_many(16, 0)
+    expect(game.score).to eq(24)
+  end  
+
+  it 'rolls perfect game' do
+    12.times { roll_strike }
+    expect(game.score).to eq(300)
+  end
+
+  it 'rolls all gutter except perfect in tenth frame' do
+    roll_many(18, 0)
+    3.times { roll_strike }
+    expect(game.score).to eq(30)
+  end
 end
